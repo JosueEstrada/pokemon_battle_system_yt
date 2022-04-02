b78c9b4f0fd485f72ab0108b68f77efe4cb27411
Josué Estrada <josue.estrada@unmsm.edu.pe>
2022-04-02 15:57:04 -0500

    add nature matrix

--- constants.py
+++ constants.py
@@ -15,3 +15,9 @@ DO_ATTACK_SELECTION = "selected_attack"
 #Attack categories
 PHYSICAL = "physical"
 SPECIAL = "special"
+
+# NATURE
+NATURE_MATRIX = [
+    {HP: 1, ATTACK: 1.1, DEFENSE: 1, SPATTACK: 0.9, SPDEFENSE: 1, SPEED: 1},
+    {HP: 1, ATTACK: 1.1, DEFENSE: 1, SPATTACK: 0.9, SPDEFENSE: 1, SPEED: 1}
+]
